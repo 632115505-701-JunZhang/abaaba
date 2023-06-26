e5f6016da6be7dab01bac6aecf5988ece504fe95
JUN ZHANG <1076353177@qq>
2023-06-26 18:42:55 +0700

    引入全局EL

--- renthouse/src/main.js
+++ renthouse/src/main.js
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 /*引入全局样式*/
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import "./assets/css/style_public.css";
+Vue.use(ElementUI);
 createApp(App).use(store).use(router).mount("#app");
